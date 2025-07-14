8c87ba1b7485d899a8677a515b45064e376f6999
ArchDevs <matthewjobava@gmail.com>
2025-07-14 12:59:02 +0400

    Initial commit

---
+++ backend/src/main/java/site/nullxweb/serversentinel/ServersentinelApplication.java
@@ -0,0 +1,13 @@
+package site.nullxweb.serversentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServersentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServersentinelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/site/nullxweb/serversentinel/ServersentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package site.nullxweb.serversentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServersentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
